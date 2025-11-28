81c2fc1beaa697877e9cf3609047fbd66056f308
WendyEducation <callizayazenteno.wendy@tecnica7.edu.ar>
2025-11-28 16:49:39 -0300

    productos

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -2,54 +2,56 @@ import pool from '../config/conexion.js'
 import * as model from '../models/products.model.js'
 
 export const getAllProduct = async (req, res) => {
-  const rows= await model.getAllProduct()
-  if(rows.error){
-     return res.status(500).send("algo salio mal")
-  }
-  (rows.length >0)? res.json(rows): res.send("NO HAY PRODUCTOSSS")
+   const rows = await model.getAllProduct()
+   if (rows.error) {
+      return res.status(500).json({ error: "Algo salió mal" });
+   }
+   (rows.length > 0) ? res.json(rows) : res.json({ message: "NO HAY PRODUCTOSSS" });
 }
 
 export const getProductById = async (req, res) => {
-    const id = req.params.id;
-    const rows = await model.getProductById(id)
-    if(rows.error){
-     return res.status(500).send("algo salio mal")
-  }
+   const id = req.params.id;
+   const rows = await model.getProductById(id)
+   if (rows.error) {
+      return res.status(500).json({ error: "Algo salió mal" });
+   }
 
-    (rows[0])? res.json(rows[0]) : res.status(404).send("Producto no existe")
+   (rows[0]) ? res.json(rows[0]) : res.status(404).json({ error: "Producto no existe" })
 }
 
 export const createProduct = async (req, res) => {
-    const values = req.body
-    const rows = model.createProduct(values)
+   const values = req.body
+   const rows = model.createProduct(values)
 
-    if(rows.error){
-     return res.status(500).send("algo salio mal")
-    }
-    res.status(201).send(`nuevo producto con id ${rows.insertId}`)
-    
+   if (rows.error) {
+      return res.status(500).json({ error: "Algo salió mal" });
+   }
+
+   res.status(201).json({
+      message: "Producto creado",
+      id: rows.insertId
+   });
 }
 
-export const updateProduct = async(req, res) => {
-     const id = req.params.id;
-     const newValues = req.body
-     const rows = model.updateProduct(newValues, id)
+export const updateProduct = async (req, res) => {
+   const id = req.params.id;
+   const newValues = req.body
+   const rows = model.updateProduct(newValues, id)
 
-      if(rows.error){
-         return res.status(500).send("algo salio mal")
-      }
+   if (rows.error) {
+      return res.status(500).json({ error: "Algo salió mal" });
+   }
 
-   (rows.affectedRows == 0)? res.status(404).send("Producto no existe") : res.send("Producto actualizado")
-
+   (rows.affectedRows == 0) ? res.status(404).json({ error: "Producto no existe" }) : res.json({ message: "Producto actualizado" });
 }
 
-export const deleteProduct = async(req, res) => {
-    const id = Number(req.params.id);
-    const rows = model.deleteProduct(id)
+export const deleteProduct = async (req, res) => {
+   const id = Number(req.params.id);
+   const rows = model.deleteProduct(id)
 
-    if(rows.error){
-      return res.status(500).send("algo salio mal")
-    }
+   if (rows.error) {
+      return res.status(500).json({ error: "Algo salió mal" });
+   }
 
-    (rows.affectedRows == 0)? res.status(404).send("Producto no existe") : res.send("PRODUCTO ELIMINADO");
-  }
+   (rows.affectedRows == 0) ? res.status(404).json({ error: "Producto no existe" }) : res.json({ message: "Producto eliminado" });
+}
